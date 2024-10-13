6b836f7c2302cd0c1794f30ae3ddf8cb24290f41
JeromeGsq <jerome.ghesquiere@gmail.com>
2024-10-13 20:58:46 +0200

    create helloworld

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+    <h1 class="text-4xl font-bold text-white">Hello World</h1>
   </main>
 </template>
+
