d1d4d892f6e517d3ed48d2462cc3dc62cf710cbf
JeromeGsq <jerome.ghesquiere@gmail.com>
2024-10-13 22:13:53 +0200

    add shadcn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main class="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
-    <h1 class="text-4xl font-bold text-white">Hello World</h1>
-  </main>
+  <main></main>
 </template>
 
+<script setup lang="ts"></script>
