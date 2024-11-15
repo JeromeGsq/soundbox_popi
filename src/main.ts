2e09806651f01e72ec91672303e7cf0d43e25626
JeromeGsq <jerome.ghesquiere@gmail.com>
2024-11-15 22:09:37 +0100

    feat: try deploy

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/index.css'
 
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import { AVPlugin } from 'vue-audio-visual'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(AVPlugin)
 
 app.mount('#app')
